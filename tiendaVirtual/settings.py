3603ad638442bd8ec84da3410c650b7bbfd75385
sebastian <sebastian.tamayo@correounivalle.edu.co>
2020-08-10 02:49:05 -0500

    Mejoras settings

--- tiendaVirtual/settings.py
+++ tiendaVirtual/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'tiendaVirtual.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd61rgtuu97u3u2',
+        'USER': 'yskvwdsixpydkm',
+        'PASSWORD': 'cef0457216c1ae34377103902d824b94d22ada7f1e6cdcd5c6e41a63554545ad',
+        'HOST': 'ec2-54-243-67-199.compute-1.amazonaws.com',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -122,6 +132,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = ((BASE_DIR, 'static'),)
 
 django_heroku.settings(locals())
