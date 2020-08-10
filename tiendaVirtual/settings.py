44415a157de8c6d69dc6b400d99a92402cd567a0
sebastian <sebastian.tamayo@correounivalle.edu.co>
2020-08-10 03:09:38 -0500

    Stati filesv2

--- tiendaVirtual/settings.py
+++ tiendaVirtual/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
